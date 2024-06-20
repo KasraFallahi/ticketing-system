dc6fa6d21ab294a28660033fedb86df15e3bd450
Kasra <kasrafa99@gmail.com>
2024-06-20 18:39:54 +0200

    ticket model added

---
+++ client/src/Ticket.jsx
@@ -0,0 +1,21 @@
+/**
+ * The Ticket type, used throughout the app.
+ * This is a constructor function, meant to be called with "new".
+ *
+ * @param ticket_id the ticket ID, a unique identifier for the ticket
+ * @param state the state of the ticket (e.g. open, closed)
+ * @param category the category of the ticket (e.g. bug, feature request)
+ * @param title the title of the ticket
+ * @param initial_text the initial text of the ticket
+ * @param submitted_at the timestamp when the ticket was submitted
+ */
+function Ticket(ticket_id, state, category, title, initial_text, submitted_at) {
+	this.ticket_id = ticket_id;
+	this.state = state;
+	this.category = category;
+	this.title = title;
+	this.initial_text = initial_text;
+	this.submitted_at = submitted_at;
+}
+
+export { Ticket };
