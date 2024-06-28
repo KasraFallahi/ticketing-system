0d2c2f09c9b545dfba4af01dc452096ae0fa0d44
Kasra <kasrafa99@gmail.com>
2024-06-28 22:49:39 +0200

    form validation added to create ticket form

--- client/src/CreateTicketForm.jsx
+++ client/src/CreateTicketForm.jsx
@@ -25,28 +25,28 @@ function CreateTicketForm(props) {
 	const handleChange = (e) => {
 		const { name, value } = e.target;
 		setFormData({ ...formData, [name]: value });
+		setFormValid({ ...formValid, [name]: true }); // Reset validation state on change
 	};
 
-	// TODO set form validation
 	// TODO goto page to accept submission of the ticket, and also show the estimated time to get a response
 	const handleSubmit = (event) => {
 		event.preventDefault();
 
 		// Validate form
-		// const titleError = validator.isEmpty(formData.title)
-		// 	? 'Email must not be empty'
-		// 	: !validator.isEmail(trimmedEmail)
-		// 	? 'Not a valid email'
-		// 	: '';
-		// const passwordValid = !validator.isEmpty(password);
+		const titleValid = !validator.isEmpty(formData.title);
+		const categoryValid = !validator.isEmpty(formData.category);
+		const descriptionValid = !validator.isEmpty(formData.description);
 
-		// if (!titleError && passwordValid) {
-		setWaiting(true);
-		props.createTicketCbk(formData, () => setWaiting(false));
-		// } else {
-		// 	settitleError(emailError);
-		// 	setPasswordValid(passwordValid);
-		// }
+		if (titleValid && categoryValid && descriptionValid) {
+			setWaiting(true);
+			props.createTicketCbk(formData, () => setWaiting(false));
+		} else {
+			setFormValid({
+				title: titleValid,
+				category: categoryValid,
+				description: descriptionValid,
+			});
+		}
 	};
 
 	return (
@@ -66,36 +66,44 @@ function CreateTicketForm(props) {
 								onChange={handleChange}
 							/>
 							<Form.Control.Feedback type="invalid">
-								title must not be empty
+								Title must not be empty
 							</Form.Control.Feedback>
 						</Form.Group>
 
-						<Form.Group>
+						<Form.Group controlId="formCategory" className="mb-3">
 							<Form.Label>Category</Form.Label>
 							<Form.Select
+								isInvalid={!formValid.category}
 								aria-label="Default select example"
-								className="mb-3"
 								name="category"
+								value={formData.category}
 								onChange={handleChange}
 							>
-								<option value="">select issue type...</option>
+								<option value="">Select issue type...</option>
 								<option value="inquiry">Inquiry</option>
 								<option value="maintenance">Maintenance</option>
 								<option value="new feature">New Feature</option>
 								<option value="administrative">Administrative</option>
 								<option value="payment">Payment</option>
 							</Form.Select>
+							<Form.Control.Feedback type="invalid">
+								Category must not be empty
+							</Form.Control.Feedback>
 						</Form.Group>
 
 						<Form.Group controlId="formDescription" className="mb-4">
 							<Form.Label>Description</Form.Label>
 							<Form.Control
+								isInvalid={!formValid.description}
 								as="textarea"
 								rows={3}
 								name="description"
 								value={formData.description}
 								onChange={handleChange}
 							/>
+							<Form.Control.Feedback type="invalid">
+								Description must not be empty
+							</Form.Control.Feedback>
 						</Form.Group>
 
 						<Button variant="primary" type="submit" disabled={waiting}>
@@ -110,9 +118,8 @@ function CreateTicketForm(props) {
 									/>{' '}
 								</>
 							) : (
-								false
+								'Submit'
 							)}
-							Submit
 						</Button>
 					</Form>
 				</Col>
